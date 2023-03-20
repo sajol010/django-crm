b98845b77e8304c051355dafd1a06a8be246fbb2
Sajol Mahmud <sajolmahmud010@gmail.com>
2023-03-21 01:30:39 +0600

    user authentication test case

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
-
+from django.test.client import Client, RequestFactory
+import json
 # Create your tests here.
+
+class UsersTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='admin', email='admin@admin.com', password='123456')
+        self.client = Client()
+
+    def test_user_authentication(self):
+
+        """ Test authentication """
+        response = self.client.post('/api/users/login', {
+            'email': 'admin@admin.com',
+            'password': '123456',
+        })
+        res = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res['success'], True)
